ed279708ec56ab2ad94cefc579010723726ec37b
KJH0215 <kimjihan0083@gmail.com>
2026-03-06 01:27:58 +0900

    앙앙

---
+++ 숫자바꾸기게임.py
@@ -0,0 +1,18 @@
+"""숫자 바꾸기 게임(if문 학습용) """
+
+while True:
+    print("숫자 맞추기 시작!")
+    a = int(input("숫자를 입력하세요:"))
+    if a>7:
+        print("땡~! 정답보다 큽니다.")
+    elif a<7:
+        print("땡~! 정답보다 작습니다.")
+    elif a ==7:
+        print("정답입니다.")
+        break
+    
+    
+    
+        
+  
+    
