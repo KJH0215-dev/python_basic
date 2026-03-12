33681d517397acf380bfaaa9fb4dfeca357c1bea
KJH0215 <kimjihan0083@gmail.com>
2026-03-12 10:06:39 +0900

    turtle_drawing

--- 반복문학습.py
+++ 반복문학습.py
@@ -1,8 +1,17 @@
 """반복문 학습용 파일"""
 
 
-a= int(input("숫자를 입력하세요:"))
+"""a= int(input("숫자를 입력하세요:"))
 b=0
 for i in range(1, a+1):
     b=b+i
-    print(b)
+    print(b)"""
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+t.color("red")
+for i  in range(360):
+    t.left(30)
+    t.forward(10)
+    t.right(30)
+    t.forward(10)
